dcf4a7704d256de68eaffc6f9dbb99b746388730
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2022-08-18 17:18:16 -0300

    feat: user table

--- MyAuth/Infrastructure/Database/Model/user.py
+++ MyAuth/Infrastructure/Database/Model/user.py
@@ -1,6 +1,8 @@
 from peewee import *
 
 from MyAuth.Infrastructure.Database.Model.base_model import BaseModel
+from MyAuth.Infrastructure.Database.Factory.abstract_connection_factory import ConnectionAbcFactory
+from MyAuth.Application.Settings.settings import Settings
 
 
 class User(BaseModel):
@@ -9,3 +11,8 @@ class User(BaseModel):
     password = CharField(column_name="Password", max_length=200)
     user_name = CharField(column_name="UserName", max_length=125)
     cellphone = CharField(column_name="Cellphone", max_length=45)
+    is_adm = BooleanField(default=False, column_name='IsAdm')
+
+    class Meta:
+        database = ConnectionAbcFactory.sql_connection(Settings.sql_db_type)
+        table_name = 'User'
